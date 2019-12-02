7af87621d0c8739ab87ed0f7b26db157e147ddfd
Adilet Momunaliev, Nina Potrebich, Sharon Lee and Will Quanstrom <sharon@>
2019-12-02 11:17:41 -0800

    add api key

--- src/coordinatesFromLocation.js
+++ src/coordinatesFromLocation.js
@@ -5,9 +5,11 @@ export class CoordinatesFromAddress {
         try {
             let response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${address}&key=${API_KEY2}`);
             let jsonifiedResponse = response.json();
-            return jsonifiedResponse; 
+            return jsonifiedResponse;
         } catch(error) {
             console.error("There was an error handling your request: " + error.message);
         }
     }
 }
+
+// Yelp API key : DrJ_qvm_NNA-jMxTlTRxnx4OsLqblKK8NxcnBeUrdLoEmni6XC5MOJXMFdwztRcSabn45bMK9rgfo9F3GMDoNlkHHf31Vy_3AaYtI8e5h8q-6Y-_0yjbLneG6GHlXXYx
